56660e31cee9341ce6994694cdad0c44ab62fe12
rickren999 <rickren999@gmail.com>
2020-09-25 22:58:05 +1200

    Onchange and suibmit function
    Add onchange function to input.
    Add submit function to submit.

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function LogIn() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeUserName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onClickSubmit = () => {
+    if (name != "" && password != "") {
+      setName(name + "222");
+      setPassword(password + "333");
+    } else {
+      window.alert("Please enter both username and password!");
+    }
+  };
+
   return (
     <div>
       <h2>Log in</h2>
       <label>
-        <b>User Name: </b>
+        <b>User Name: {name}</b>
       </label>
-      <input type="text" />
+      <input type="text" value={name} onChange={onChangeUserName} />
       <br />
       <br />
       <label>
-        <b>Password: </b>
+        <b>Password: {password}</b>
       </label>
-      <input type="Password" />
+      <input type="Password" value={password} onChange={onChangePassword} />
       <br />
       <br />
-      <button>
+      <button onClick={onClickSubmit}>
         <b>Submit</b>
       </button>
     </div>
